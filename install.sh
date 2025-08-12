5c09337bd389e6830f5901b5d335f45058a42418
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-08-12 22:15:57 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 
-qemu-img create -f qcow2 /opt/images/ubuntu.img 8G
+mkdir /opt/images/
+
+wget -nc -P /opt/images/ https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso 
+
+qemu-img create -f qcow2 /opt/images/ubuntu.img 20G
 
 kvm -hda /opt/images/ubuntu.img \
     -cdrom /opt/images/ubuntu-24.04.3-desktop-amd64.iso \
