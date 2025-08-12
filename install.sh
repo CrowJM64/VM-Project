575a92973a4855f010ecd5afb762253307b22c76
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-08-12 21:52:32 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 
-kvm -hda Ubuntu.img \
-    -cdrom ubuntu-24.04.3-desktop-amd64.iso \
+qemu-img create -f qcow2 /opt/images/ubuntu.img 8G
+
+kvm -hda /opt/images/ubuntu.img \
+    -cdrom /opt/images/ubuntu-24.04.3-desktop-amd64.iso \
     -m 8192 \
     -net nic \
     -net user \
