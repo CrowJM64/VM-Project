25e47e26f7411eae505c8be64f817c620bec8318
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-08-13 08:18:55 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@ mkdir /opt/images/
 
 wget -nc -P /opt/images/ https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso 
 
-qemu-img create -f qcow2 /opt/images/ubuntu.img 20G
+# vmname = "USER INPUT"
 
-kvm -hda /opt/images/ubuntu.img \
+read -p "Enter the desired name of the VM: " vmname
+
+qemu-img create -f qcow2 /opt/images/$vmname.img 20G
+
+kvm -hda /opt/images/$vmname.img \
     -cdrom /opt/images/ubuntu-24.04.3-desktop-amd64.iso \
     -m 8192 \
     -net nic \
