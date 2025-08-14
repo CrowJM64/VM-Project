3c38bb8a6ab43f4e214911ad5fe967e1ab5034f7
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-08-14 08:31:22 +0100

    Added options for imagepath, HDDsize and ramsize with defaults.

--- install.sh
+++ install.sh
@@ -1,17 +1,38 @@
+#  = "USER INPUT"
 
-mkdir /opt/images/
+#VM Installation Location
+read -p 'Where would you like the VM Image to be stored (default /opt/images/): ' -e -i '/opt/images' imagepath
+printf "You have chosen $imagepath
+"
+mkdir -p $imagepath
+
+#Choose which ISO from the list.
 
 wget -nc -P /opt/images/ https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso 
 
+
+#Name the VM
 # vmname = "USER INPUT"
+read -p 'Enter the desired name of the VM: ' vmname
 
-read -p "Enter the desired name of the VM: " vmname
 
-qemu-img create -f qcow2 /opt/images/$vmname.img 20G
+#RAM Size
+# ram = "USER INPUT"
+read -p 'Enter the desired amount of RAM in MB (default 8192): ' -e -i '8192'  ram
+printf "Assigning the VM with $ram MB RAM
+"
+
+#HDD Size
+read -p 'Enter the desired storage size assigned to the VM (Default 16G): ' -e -i '16G'  hddsize
+printf "Assigning the VM with a $hddsize storage allocation.
+"
+
+
+qemu-img create -f qcow2 /opt/images/$vmname.img $hddsize
 
 kvm -hda /opt/images/$vmname.img \
     -cdrom /opt/images/ubuntu-24.04.3-desktop-amd64.iso \
-    -m 8192 \
+    -m $ram \
     -net nic \
     -net user \
 #    -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0
