c4ab1d57ac02361e7cbc726c7e78a51d656986ee
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-08-14 21:06:00 +0100

    Fixed multiple choice ISO booting. ADded breakpoints.

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-#  = "USER INPUT"
+#!/bin/bash
 
-apt-get install -y -v qemu-system
+
+echo -e "\e[36m=====================================\e[0m"
+echo -e "\e[1m\e[34m        VM Creation Script        \e[0m"
+echo -e "\e[36m=====================================\e[0m"
+
+echo -e "Installing QEMU System and KVM..."
+apt-get install -q -y qemu-system
 
 #VM Installation Location
 read -p 'Where would you like the VM Image to be stored (default /opt/images/): ' -e -i '/opt/images' imagepath
@@ -11,18 +17,30 @@ mkdir -p $imagepath
 #Choose which ISO from the list.
 
 PS3='Please choose the desired OS: '
-options=("Ubuntu" "Fedora" "Rocky 9" "Quit")
+options=("Ubuntu" "Fedora" "Rocky 10" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Ubuntu")
-            wget -nc -P /opt/images/ https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso
+            echo -e "Downloading \e[1m\e[34m Ubuntu 24.04.3 ISO...\e[0m"
+            wget -nc -nv --show-progress --progress=bar -P $imagepath https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso
+            oschosen="Ubuntu"
+            iso="$imagepath/ubuntu-24.04.3-desktop-amd64.iso"
+            break
             ;;
         "Fedora")
-            echo "you chose choice 2"
+            echo -e "Downloading \e[1m\e[34m Fedora 42 ISO...\e[0m"
+            wget -nc -nv --show-progress --progress=bar -P $imagepath https://download.fedoraproject.org/pub/fedora/linux/releases/42/Workstation/x86_64/iso/Fedora-Workstation-Live-42-1.1.x86_64.iso
+            oschosen="Fedora"
+            iso="$imagepath/Fedora-Workstation-Live-42-1.1.x86_64.iso"
+            break
             ;;
-        "Rocky 9")
-            echo "you chose choice $REPLY which is $opt"
+        "Rocky 10")
+            echo -e "Downloading \e[1m\e[34m Rocky Linux 10 ISO...\e[0m"
+            wget -nc -nv --show-progress --progress=bar -P $imagepath https://download.rockylinux.org/pub/rocky/10/isos/x86_64/Rocky-10.0-x86_64-dvd1.iso
+            oschosen="Rocky 10"
+            iso=$imagepath/Rocky-10.0-x86_64-dvd1.iso
+            break
             ;;
         "Quit")
             break
@@ -33,16 +51,12 @@ done
 
 
 
-wget -nc -P /opt/images/ https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso 
-
-
 #Name the VM
-# vmname = "USER INPUT"
-read -p 'Enter the desired name of the VM: ' vmname
-
+read -p 'Enter the desired name of the VM: ' -e -i "$oschosen" vmname
+printf "Creating the image: $vmname
+"
 
 #RAM Size
-# ram = "USER INPUT"
 read -p 'Enter the desired amount of RAM in MB (default 8192): ' -e -i '8192'  ram
 printf "Assigning the VM with $ram MB RAM
 "
@@ -53,10 +67,10 @@ printf "Assigning the VM with a $hddsize storage allocation.
 "
 
 
-qemu-img create -f qcow2 /opt/images/$vmname.img $hddsize
+qemu-img create -f qcow2 $imagepath$vmname.img $hddsize
 
-kvm -hda /opt/images/$vmname.img \
-    -cdrom /opt/images/ubuntu-24.04.3-desktop-amd64.iso \
+kvm -hda $imagepath$vmname.img \
+    -cdrom $iso \
     -m $ram \
     -net nic \
     -net user \
