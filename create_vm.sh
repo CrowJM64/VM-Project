2f89e7ec7dcea5d4b25841a1262056f267f3c7fd
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-08-16 20:38:35 +0100

    Updated Create VM Script,
    Half fixed resume VM script

--- create_vm.sh
+++ create_vm.sh
@@ -22,28 +22,29 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Ubuntu")
-            echo -e "Downloading \e[1m\e[34m Ubuntu 24.04.3 ISO...\e[0m"
+            echo -e "Downloading\e[1m\e[34m Ubuntu 24.04.3 ISO\e[0m if needed..."
             wget -nc -nv --show-progress --progress=bar -P $imagepath/ISO https://mirror.server.net/ubuntu-releases/24.04.3/ubuntu-24.04.3-desktop-amd64.iso
             oschosen="Ubuntu"
             iso="$imagepath/ISO/ubuntu-24.04.3-desktop-amd64.iso"
             break
             ;;
         "Fedora")
-            echo -e "Downloading \e[1m\e[34m Fedora 42 ISO...\e[0m"
+            echo -e "Downloading\e[1m\e[34m Fedora 42 ISO\e[0m if needed..."
             wget -nc -nv --show-progress --progress=bar -P $imagepath/ISO/ https://download.fedoraproject.org/pub/fedora/linux/releases/42/Workstation/x86_64/iso/Fedora-Workstation-Live-42-1.1.x86_64.iso
             oschosen="Fedora"
             iso="$imagepath/ISO/Fedora-Workstation-Live-42-1.1.x86_64.iso"
             break
             ;;
         "Rocky 10")
-            echo -e "Downloading \e[1m\e[34m Rocky Linux 10 ISO...\e[0m"
+            echo -e "Downloading\e[1m\e[34m Rocky Linux 10 ISO\e[0m if needed..."
             wget -nc -nv --show-progress --progress=bar -P $imagepath/ISO/ https://download.rockylinux.org/pub/rocky/10/isos/x86_64/Rocky-10.0-x86_64-dvd1.iso
             oschosen="Rocky 10"
             iso=$imagepath/ISO/Rocky-10.0-x86_64-dvd1.iso
             break
             ;;
         "Quit")
-            break
+            echo -e "Exiting VM creation script."
+            exit 1
             ;;
         *) echo "invalid option $REPLY";;
     esac
@@ -67,6 +68,8 @@ printf "Assigning the VM with a $hddsize storage allocation.
 "
 
 qemu-img create -f qcow2 $imagepath/Images/$vmname.img $hddsize
+
+chmod 744 $imagepath/Images/$vmname.img
 
 kvm -hda $imagepath/Images/$vmname.img \
     -cdrom $iso \
