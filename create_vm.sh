3b3c240489ce26e351953399fe4273ab55cdf5e0
Josh Murphy <joshua.murphy1998@hotmail.com>
2025-09-06 18:10:15 +0100

    modified:   create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -16,7 +16,7 @@ mkdir -p $imagepath $imagepath/ISO $imagepath/Images
 #Choose which ISO from the list.
 
 PS3='Please choose the desired OS: '
-options=("Ubuntu" "Fedora" "Rocky 10" "Windows Server 2022" "Bazzite" "LinuxMint" "Quit")
+options=("Ubuntu" "Fedora" "Rocky10" "WindowsServer2022" "Bazzite" "LinuxMint" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -34,14 +34,14 @@ do
             iso="$imagepath/ISO/Fedora-Workstation-Live-42-1.1.x86_64.iso"
             break
             ;;
-        "Rocky_10")
+        "Rocky10")
             echo -e "Downloading\e[1m\e[34m Rocky Linux 10 ISO\e[0m if needed..."
             wget -nc -nv --show-progress --progress=bar -P $imagepath/ISO/ https://download.rockylinux.org/pub/rocky/10/isos/x86_64/Rocky-10.0-x86_64-dvd1.iso
             oschosen="Rocky 10"
             iso=$imagepath/ISO/Rocky-10.0-x86_64-dvd1.iso
             break
             ;;
-        "Windows_Server_2022")
+        "WindowsServer2022")
             echo -e "Downloading\e[1m\e[34m Windows Server 2022 ISO\e[0m if needed..."
             wget -nc -nv --show-progress --progress=bar -P $imagepath/ISO/ https://software-static.download.prss.microsoft.com/sg/download/888969d5-f34g-4e03-ac9d-1f9786c66749/SERVER_EVAL_x64FRE_en-us.iso
             oschosen="Windows_Server_2022"
@@ -51,7 +51,7 @@ do
         "Bazzite")
             echo -e "Downloading\e[1m\e[34m Bazzite ISO\e[0m if needed..."
             wget -nc -nv --show-progress --progress=bar -P $imagepath/ISO/ https://download.bazzite.gg/bazzite-deck-nvidia-stable-amd64.iso
-            oschosen="Windows_Server_2022"
+            oschosen="Bazzite"
             iso=$imagepath/ISO/bazzite-deck-nvidia-stable-amd64.iso
             break
             ;;
